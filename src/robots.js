ff19b86aac12295357fd53bbaec2358e3d0f5e13
mariomartell <46970459+mariomartell@users.noreply.github.com>
2020-01-01 23:24:44 -0700

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Ryan French',
+    username: 'Bret',
+    email: 'Ryan@French.biz'
+  },
+  {
+    id: 2,
+    name: 'Tommy Nguyen',
+    username: 'Antonette',
+    email: 'Tommy@Nguyen.tv'
+  },
+  {
+    id: 3,
+    name: 'Brandon Medina',
+    username: 'Samantha',
+    email: 'Brandon@Medina.net'
+  },
+  {
+    id: 4,
+    name: 'Carlos Chavez',
+    username: 'Karianne',
+    email: 'Dylan@Lucas.org'
+  },
+  {
+    id: 5,
+    name: 'El Guicho',
+    username: 'Kamren',
+    email: 'El@Guicho.ca'
+  },
+  {
+    id: 6,
+    name: 'Kevin Leparksi',
+    username: 'Leopoldo_Corkery',
+    email: 'Mr Ford@email.info'
+  },
+  {
+    id: 7,
+    name: 'Keska',
+    username: 'Elwyn.Skiles',
+    email: 'Keska@email.biz'
+  },
+  {
+    id: 8,
+    name: 'Ceci',
+    username: 'Maxime_Nienow',
+    email: 'Ceci@email.me'
+  },
+  {
+    id: 9,
+    name: 'Maya',
+    username: 'Delphine',
+    email: 'Maya@email.io'
+  },
+  {
+    id: 10,
+    name: 'Dylan Lucas',
+    username: 'Moriah.Stanton',
+    email: 'Dylan@Lucas.biz'
+  }
+];
